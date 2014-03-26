6a83e35e0343c40ffe6df03e158ddc6dd10404e8
mdxs <van.wijgerden@mdxs.net>
2014-03-27 00:20:54 +0100

    Prepare example for p4 and p5 searches

--- example.py
+++ example.py
@@ -1,6 +1,19 @@
-from pyPostcode import Api
+import pyPostcode
 
-postcodeapi = Api('{YOUR_API_KEY}')
-result_street = postcodeapi.getaddress('1011AC') # use p6 search
-result_address = postcodeapi.getaddress('1011AC', 154) # use address search
-print result_street._data, result_address._data
+postcodeapi = pyPostcode.Api('{YOUR_API_KEY}')
+
+print '\n===', 'using p4 search:'
+result = postcodeapi.getaddress('5041')
+print ":", result._data if result else 'None'
+
+print '\n===', 'using p5 search:'
+result = postcodeapi.getaddress('5041E')
+print ":", result._data if result else 'None'
+
+print '\n===', 'using p6 search:'
+result = postcodeapi.getaddress('5041EB')
+print ":", result._data if result else 'None'
+
+print '\n===', 'using address search (p6 + house number):'
+result = postcodeapi.getaddress('5041EB', 21) # use address search
+print ":", result._data if result else 'None'
